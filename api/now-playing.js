395a2cdcddccb2085864deb853c1aa6eba5e723e
juliasveen <jds21k@fsu.edu>
2026-04-27 22:19:35 -0400

    fix vercel x spotify deployment

--- api/now-playing.js
+++ api/now-playing.js
@@ -24,6 +24,8 @@ const getAccessToken = async () => {
 };
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   try {
     const { access_token } = await getAccessToken();
     
